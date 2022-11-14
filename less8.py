022d1d8bcab2640e87c152160410ffb1fccbc501
k4t4qq <116014310+k4t4qq@users.noreply.github.com>
2022-11-14 18:13:57 +0200

    Add files via upload

--- less8.py
+++ less8.py
@@ -1,38 +1,44 @@
-# ! logging
-# import logging
+# import urllib.request
+# import requests
 #
-# logging.basicConfig(level = logging.DEBUG,
-# 		    filename = "logs.log",
-# 		    filemode = "a",
-# 		    format = "We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
-# logging.debug("debug")
-# logging.info("info")
-# logging.warning("warning")
-# logging.error("error")
-# logging.critical("critical")
-# try:
-#     print(10/10)
-# except Exception:
-#     logging.exception("Exception")
+# # opener = urllib.requests.build.build_opener()
+# # response = opener.open("https://httpbin.org/get")
+# # print(response.read())
+# response = requests.get("https://httpbin.org/get")
+# # print(response.content())
+# print(f"Datatype - {type(response.content)}")
+# print(response.text)
+# print(f"Datatype - {type(response.text)}")
 #
-# 0.5! ASSERT
-# assert 2+2==5,"ne rabotaet"
-# assert func(), "func ne rabitea"
+# response = requests.post("https://httpbin.org/post",
+#                         data = "Nasha zapis",
+#                         headers = {"h1" : "Nasha zapiska"})
+# print(response.text)
 #
-# !! DOCTEST
-# """
-# >>> 2+2
-# 5
-# """
-# IF __name == "__main__":
-#     import doctest
-#     doctest.testmod()
+# import requests
 #
-#!!! unittest
-def adder(*args, **kwargs):
-    result = 0
-    for _ in args:
-        result += _
-    for _ in kwargs.values():
-        result += _
-    return result
+# res_parse_list = []
+# response = requests.get("https://coinmarketcap.com/")
+#
+# response_text = response.text
+# response_parse= response.text.split("<span>")
+# for parse_elem_1 in response_parse:
+#     if parse_elem_1.startswith("$"):
+#         for parse_elem_2 in parse_elem_1.split("</span>"):
+#             if parse_elem_2.startswith("$") and parse_elem_2[1].isdigit():
+#                 # print(parse_elem_2)
+#                 res_parse_list.append(parse_elem_2)
+# btc = res_parse_list[0]
+# print(btc)
+
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://privatbank.ua/")
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, features="html.parser")
+    soup_list = soup.find_all("td", {"id" : "USD_sell"})
+for elem in soup_list:
+    print(elem.text)
+    # res = soup_list[0].find("span")
+    # print(res.text)
